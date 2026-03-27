21d59329284398ccdde9e8586ced09e56cf82933
sdouf5054 <sdouf5054@gmail.com>
2026-03-27 20:12:00 +0900

    realtek support

--- port/windows-winusb/main.c
+++ port/windows-winusb/main.c
@@ -168,10 +168,6 @@ int main(int argc, const char * argv[]){
 
     // init HCI
     hci_init(hci_transport_usb_instance(), NULL);
-    // Force Realtek chipset for TP-Link UB500 (RTL8761B)
-    btstack_chipset_realtek_set_product_id(0x0604);
-    hci_set_chipset(btstack_chipset_realtek_instance());
-    hci_enable_custom_pre_init();
 
     // register known Realtek USB Controllers
     uint16_t realtek_num_controllers = btstack_chipset_realtek_get_num_usb_controllers();
